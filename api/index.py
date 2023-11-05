d4683f5ee12be26350d26577d1ec94dccb34580a
LusineHairapetian <lousinehairapetian@gmail.com>
2023-11-05 23:57:39 +0100

    Interactive map with waves data

--- api/index.py
+++ api/index.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import xarray as xr
+import numpy as np
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+@app.get("/api/getData")
+def getTheHighestWave(longitude: str, latitude: str):
+  wavesData = xr.open_dataset("api/waves_2019-01-01.nc")
+  wavesOnPoint = wavesData.interp(longitude=float(longitude), latitude=float(latitude))
+  maxWave = np.nanmax(wavesOnPoint["hmax"].values)
+  maxWaveValid = 0 if np.isnan(maxWave) else maxWave
+  return {maxWaveValid}
---
+++ components/Map.tsx
@@ -0,0 +1,79 @@
+import "mapbox-gl/dist/mapbox-gl.css"
+import { useMemo, useState } from 'react'
+import Map, { Source, Layer, Marker } from 'react-map-gl'
+import type { CircleLayer } from 'react-map-gl'
+import type { FeatureCollection } from 'geojson'
+
+export interface MarkersStateProperties {
+    longitude: number;
+    latitude: number;
+}
+
+export default function (props: any) {
+    const geojson = useMemo((): FeatureCollection => {
+        const geoJson = {
+            type: 'FeatureCollection',
+            features: [{
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                    type: 'Point', 
+                    coordinates: [180, 70]
+                }
+            }]
+        }
+        for (let lng = -180; lng < 180; lng += 0.5) {
+            for (let lat = -60; lat <70; lat += 0.5) {
+                geoJson.features.push({
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'Point', 
+                        coordinates: [lng, lat]
+                    }
+                })
+            }
+        }
+        return geoJson;
+    }, [])
+
+    const [markers, setMarkers] = useState<MarkersStateProperties[]>([])
+
+    function handleClick({ lngLat }: mapboxgl.MapLayerMouseEvent) {
+        const {lng, lat} = lngLat
+        setMarkers([...markers, { longitude: lng, latitude: lat }])
+        props.getClickCoordinates({ longitude: lng, latitude: lat })
+    }
+    
+    const layerStyle: CircleLayer = {
+        id: 'point',
+        type: 'circle',
+        paint: {
+          'circle-radius': 1,
+          'circle-color': 'red'
+        }
+    }
+
+    return (
+        <Map
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+            initialViewState={{
+                longitude: 11,
+                latitude: 60,
+                zoom: 10,
+            }}
+            onClick={handleClick}
+            minZoom={3}
+            renderWorldCopies={false}
+            style={{ width: "800px", height: "100%" }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+        >
+            <Source id="my-data" type="geojson" data={geojson}>
+                <Layer {...layerStyle} />
+            </Source>
+            {markers.length && markers.map((marker, i) => (
+                <Marker {...marker} key={i}>{i+1}+</Marker>
+            ))}
+        </Map>
+    )
+}
