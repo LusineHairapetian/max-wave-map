3813fec4650b2bec4f83fb417c60250ad28fcd1c
LusineHairapetian <lousinehairapetian@gmail.com>
2023-11-06 15:41:07 +0100

    fix response json encoder issue

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/api/get_data")
 def getTheHighestWave(longitude: str, latitude: str):
   waves_data = xr.load_dataset("api/waves_2019-01-01.nc")
-  waves_on_point = waves_data.sel(longitude=float(longitude), latitude=float(latitude), method="nearest", tolerance=0.7)
+  waves_on_point = waves_data.sel(longitude=float(longitude), latitude=float(latitude), method="nearest", tolerance=0.35)
   max_wave = np.nanmax(waves_on_point["hmax"].values)
-  max_wave_valid = 0 if np.isnan(max_wave) else max_wave
+  max_wave_valid = 0 if np.isnan(max_wave) else float(max_wave)
   return {max_wave_valid}
