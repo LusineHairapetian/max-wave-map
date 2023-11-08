b07a3a404b3aeb6f0bc14da3f56606ad4e428e37
LusineHairapetian <lousinehairapetian@gmail.com>
2023-11-08 13:42:04 +0100

    Validate coordinates in api

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import xarray as xr
 import numpy as np
 
@@ -6,8 +6,22 @@ app = FastAPI()
 
 @app.get("/api/get_data")
 def getTheHighestWave(longitude: str, latitude: str):
+  try:
+    lngNum = float(longitude)
+    latNum = float(latitude)
+
+    if lngNum < -180 or lngNum >= 180 or latNum < -60 or latNum > 70:
+      raise Exception
+
+  except:
+    raise HTTPException(status_code=400, detail="Invalid coordinates requested")
+  
   waves_data = xr.load_dataset("api/waves_2019-01-01.nc")
+
   waves_on_point = waves_data.sel(longitude=float(longitude), latitude=float(latitude), method="nearest", tolerance=0.35)
   max_wave = np.nanmax(waves_on_point["hmax"].values)
-  max_wave_valid = 0 if np.isnan(max_wave) else float(max_wave)
-  return {max_wave_valid}
+
+  if np.isnan(max_wave):
+    raise HTTPException(status_code=400, detail="No data found")
+
+  return {"max_wave": float(max_wave)}
--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,38 @@ import Map, { MarkersStateProperties } from '../components/Map'
 
 const inter = Inter({ subsets: ['latin'] })
 
-type ClickDataType = MarkersStateProperties & {
-  maxWave: number
+type ClickDataType = {
+  longitude: number,
+  latitude: number,
+  maxWave: number,
+  isError: boolean
 }
 
 export default function Home() {
   const [clickData, setClickData] = useState<ClickDataType[]>([]);
 
-  async function getClickCoordinatesHandler({longitude, latitude}: MarkersStateProperties) {
+  async function getClickCoordinatesHandler({ longitude, latitude }: MarkersStateProperties) {
     const coordinatesStringified = {
-      longitude: longitude.toString(), 
+      longitude: longitude.toString(),
       latitude: latitude.toString()
     };
     const response = await fetch(`/api/get_data?${new URLSearchParams(coordinatesStringified)}`);
-    const maxWave = await response.json();
-    setClickData([...clickData, {longitude, latitude, maxWave}])
+    let maxWave = 0;
+    let isError = true;
+
+    if(response.status === 200) {
+      maxWave = (await response.json()).max_wave.toFixed(2);
+      isError = false;
+    }
+    
+    setClickData([
+      ...clickData,
+      {
+        longitude: +longitude.toFixed(4),
+        latitude: +longitude.toFixed(4),
+        maxWave,
+        isError
+      }])
   }
 
   return (
@@ -30,15 +47,15 @@ export default function Home() {
       <Map getClickCoordinates={getClickCoordinatesHandler} />
       <div className="p-5 border bg-white text-sm flex flex-col overflow-hidden">
         <div className="p-5 border bg-amber-100 leading-4 text-sm">
-          <h6 className="font-bold">About the map</h6><br/>
-          <p>🔴 The red dots on the map show data points</p><br/>
-          <p className="text-xs">The closer you click to them the more accurate result data will be</p><br/>
+          <h6 className="font-bold">About the map</h6><br />
+          <p>🔴 The red dots on the map show data points</p><br />
+          <p className="text-xs">The closer you click to them the more accurate result data will be</p><br />
           <p>➕ The plus sign on the map shows all marked points</p>
         </div>
         <div className="p-5 border leading-4 text-sm overflow-y-auto">
-          <h6 className="font-bold">Listing the waves</h6><br/>
-          {clickData.length && clickData.map(({longitude, latitude, maxWave}, i) => <p key={i} className="text-xs bg-slate-100 mb-1">
-            {i+1}. On [{longitude},{latitude}] highest wave was around {maxWave} meters
+          <h6 className="font-bold">Listing the waves</h6><br />
+          {!!clickData.length && clickData.map(({ longitude, latitude, maxWave, isError }, i) => <p key={i} className={`text-xs ${isError ? 'bg-red-100' : 'bg-slate-100'} mb-1`}>
+            {i + 1}. On [{longitude},{latitude}] highest wave was {isError ? 'not found': `around ${maxWave} meters`}
           </p>)}
         </div>
       </div>
